66f8ce108680953c115fc2274f573ae383395c78
tzy <zhaoyangtang@jieyuechina.com>
2019-05-23 18:07:04 +0800

    listview、 gridview 以及冲突

---
+++ lib/ui/pages/list_page.dart
@@ -0,0 +1,214 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/common/component_index.dart';
+import 'package:flutter_app/ui/pages/page_index.dart';
+
+class ListPage extends StatefulWidget{
+  LoginModel loginModel;
+  ListPage({this.loginModel});
+
+  @override
+  State<StatefulWidget> createState() {
+    return new LobbyPageState();
+  }
+
+}
+
+class ListPageState extends State<ListPage>{
+  String tag = "ListPage";
+  RefreshController _refreshController;
+
+  var _items = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _refreshController = RefreshController();
+    // 如果你需要开始就请求一次刷新
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _refreshController.requestRefresh();
+    });
+  }
+
+  void _onRefresh(){
+    LogUtil.e("_onRefresh ", tag: tag);
+    _items.clear();
+    var length = _items.length;
+    for(int i=length+1; i < length+15; i++){
+      _items.add(Model('id:'+i.toString(), "title:"+i.toString(), "pic:"+i.toString(), i,
+          i, i, "des:"+i.toString(), "lunar:"+i.toString()));
+    }
+    setState(() {
+
+    });
+    _refreshController.refreshCompleted();
+
+    /*.  after the data return,
+        use _refreshController.refreshComplete() or refreshFailed() to end refreshing
+   */
+  }
+
+  void _onLoading(){
+    LogUtil.e("_onLoading ", tag: tag);
+    var length = _items.length;
+    for(int i=length+1; i < length+15; i++){
+      _items.add(Model('id:'+i.toString(), "title:"+i.toString(), "pic:"+i.toString(), i,
+          i, i, "des:"+i.toString(), "lunar:"+i.toString()));
+    }
+    setState(() {
+
+    });
+    _refreshController.loadComplete();
+    /*
+        use _refreshController.loadComplete() or loadNoData() to end loading
+   */
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+//        title: new Text(IntlUtil.getString(context, )),
+        title: new Text(
+            widget.loginModel.sellerInfo.sellerName.isNotEmpty ?widget.loginModel.sellerInfo.sellerName: "大堂",
+          style: new TextStyle(color: Colors.black),
+        ),
+        backgroundColor: Colours.color_EAEAEA,
+        centerTitle: true,
+        leading: IconButton(
+            icon: Icon(Icons.navigate_before,
+            color: Colours.green_62,
+              size: 38,
+            ),
+            onPressed: ((){
+              Navigator.pop(context);
+            }),
+        ),
+        actions: <Widget>[new IconButton(
+            icon: new Icon(Icons.more_horiz,
+            size: 28,),
+            onPressed: null,
+        )],
+      ),
+      body: new SmartRefresher(
+        enablePullDown: true,
+        enablePullUp: true,
+//        header: defaultTargetPlatform == TargetPlatform.iOS?WaterDropHeader():WaterDropMaterialHeader(),
+        controller: _refreshController,
+        onRefresh: _onRefresh,
+        onLoading: _onLoading,
+        child:  new ListView.builder(itemCount: _items.length, itemBuilder: itemView),
+      ),
+
+//      new ListView.builder(
+//        padding: new EdgeInsets.all(5.0),
+//        itemExtent: 50.0,
+//          itemBuilder: (BuildContext context,int index){
+//            return new Text("text $index");
+//          },
+//      ),
+//      new ListView(
+//        children: <Widget>[
+//          new Container(
+//            margin: EdgeInsets.fromLTRB(0, 15, 0, 0),
+//            child: new Row(
+//              children: <Widget>[
+//                new InkWell(
+//                  child: new Image.asset(
+//                    Utils.getImgPath('ico_refresh'),
+//                    width: 30,
+//                  ),
+//                  onTap: refreshAll,
+//                ),
+//                new Container(
+//                  padding: EdgeInsets.fromLTRB(30, 0, 30, 0),
+//                  child: new MaterialButton(
+//                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(5))),
+//                    color: Colours.color_49B2B6,
+//                    textColor: Colors.white,
+//                    child: new Text('统计汇总'),
+//                    onPressed: statistics,
+//                  ),
+//                ),
+//                new MaterialButton(
+//                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(5))),
+//                  color: Colours.color_49B2B6,
+//                  textColor: Colors.white,
+//                  child: new Text('退出'),
+//                  onPressed: loginOut,
+//                )
+//              ],
+//              mainAxisAlignment: MainAxisAlignment.center,
+//            ),
+//
+//          )
+//        ],
+//      ),
+
+    );
+  }
+  Widget itemView(BuildContext context, int index) {
+    Model model = this._items[index];
+    //设置分割线
+    if (index.isOdd) return new Divider(height: 2.0);
+    return new Container(
+        color: Color.fromARGB(0x22, 0x49, 0xa9, 0x8d),
+        child: new Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: new Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: new Column(
+                  children: <Widget>[
+                    new Row(
+                      children: <Widget>[
+                        new Text('${model.year}年${model.month}月${model.day}日',
+                            style: new TextStyle(fontSize: 15.0)),
+                        new Text('(${model.lunar})',
+                            style: new TextStyle(fontSize: 15.0)),
+                      ],
+                    ),
+                    new Center(
+                      heightFactor: 6.0,
+                      child: new Text("${model.title}",
+                          style: new TextStyle(fontSize: 17.0)),
+                    )
+                  ],
+                ))));
+  }
+
+
+
+
+
+
+  void dispose(){
+    _refreshController.dispose();
+    super.dispose();
+  }
+
+
+  void refreshAll(){
+    LogUtil.e("刷新 ", tag: tag);
+  }
+  void loginOut(){
+    LogUtil.e("退出 ", tag: tag);
+    Navigator.of(context).pop('loginOut');
+  }
+  void statistics(){
+    LogUtil.e("statistics ", tag: tag);
+  }
+
+}
+
+class Model {
+  String _id;
+  String title;
+  String pic;
+  int year;
+  int month;
+  int day;
+  String des;
+  String lunar;
+
+  Model(this._id, this.title, this.pic, this.year, this.month, this.day,
+      this.des, this.lunar);
+}
--- lib/ui/pages/lobby_page.dart
+++ lib/ui/pages/lobby_page.dart
@@ -1,20 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/common/component_index.dart';
-import 'package:flutter_app/ui/pages/page_index.dart';
 
-class LobbyPage extends StatefulWidget{
+class LobbyPage extends StatefulWidget {
   LoginModel loginModel;
+
   LobbyPage({this.loginModel});
 
   @override
   State<StatefulWidget> createState() {
     return new LobbyPageState();
   }
-
 }
 
-class LobbyPageState extends State<LobbyPage>{
+class LobbyPageState extends State<LobbyPage> {
   String tag = "LobbyPage";
+  List<String> datas;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    datas = getDataList();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,27 +29,36 @@ class LobbyPageState extends State<LobbyPage>{
       appBar: new AppBar(
 //        title: new Text(IntlUtil.getString(context, )),
         title: new Text(
-            widget.loginModel.sellerInfo.sellerName.isNotEmpty ?widget.loginModel.sellerInfo.sellerName: "大堂",
+          widget.loginModel.sellerInfo.sellerName.isNotEmpty
+              ? widget.loginModel.sellerInfo.sellerName
+              : "大堂",
           style: new TextStyle(color: Colors.black),
         ),
         backgroundColor: Colours.color_EAEAEA,
         centerTitle: true,
         leading: IconButton(
-            icon: Icon(Icons.navigate_before,
+          icon: Icon(
+            Icons.navigate_before,
             color: Colours.green_62,
-              size: 38,
-            ),
-            onPressed: ((){
-              Navigator.pop(context);
-            }),
+            size: 38,
+          ),
+          onPressed: (() {
+            Navigator.pop(context);
+          }),
         ),
-        actions: <Widget>[new IconButton(
-            icon: new Icon(Icons.more_horiz,
-            size: 28,),
+        actions: <Widget>[
+          new IconButton(
+            icon: new Icon(
+              Icons.more_horiz,
+              size: 28,
+            ),
             onPressed: null,
-        )],
+          )
+        ],
       ),
       body: new ListView(
+        shrinkWrap: true,
+        primary: false,
         children: <Widget>[
           new Container(
             margin: EdgeInsets.fromLTRB(0, 15, 0, 0),
@@ -58,7 +74,8 @@ class LobbyPageState extends State<LobbyPage>{
                 new Container(
                   padding: EdgeInsets.fromLTRB(30, 0, 30, 0),
                   child: new MaterialButton(
-                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(5))),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(5))),
                     color: Colours.color_49B2B6,
                     textColor: Colors.white,
                     child: new Text('统计汇总'),
@@ -66,7 +83,8 @@ class LobbyPageState extends State<LobbyPage>{
                   ),
                 ),
                 new MaterialButton(
-                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(5))),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(5))),
                   color: Colours.color_49B2B6,
                   textColor: Colors.white,
                   child: new Text('退出'),
@@ -75,24 +93,65 @@ class LobbyPageState extends State<LobbyPage>{
               ],
               mainAxisAlignment: MainAxisAlignment.center,
             ),
-
-          )
+          ),
+          new GridView.builder(
+              shrinkWrap: true,
+              primary: false,
+              itemCount: datas.length,
+              //SliverGridDelegateWithFixedCrossAxisCount 构建一个横轴固定数量Widget
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  //横轴元素个数
+                  crossAxisCount: 3,
+                  //纵轴间距
+                  mainAxisSpacing: 20.0,
+                  //横轴间距
+                  crossAxisSpacing: 10.0,
+                  //子组件宽高长度比例
+                  childAspectRatio: 1.0),
+              itemBuilder: (BuildContext context, int index) {
+                //Widget Function(BuildContext context, int index)
+                return getItemContainer(datas[index]);
+              }),
         ],
-
       ),
-
     );
   }
 
-  void refreshAll(){
+  List<String> getDataList() {
+    List<String> list = [];
+    for (int i = 0; i < 100; i++) {
+      list.add(i.toString());
+    }
+    return list;
+  }
+
+  List<Widget> getWidgetList() {
+    return getDataList().map((item) => getItemContainer(item)).toList();
+  }
+
+  Widget getItemContainer(String item) {
+    return Container(
+      width: 5.0,
+      height: 5.0,
+      alignment: Alignment.center,
+      child: Text(
+        item,
+        style: TextStyle(color: Colors.white, fontSize: 20),
+      ),
+      color: Colors.blue,
+    );
+  }
+
+  void refreshAll() {
     LogUtil.e("刷新 ", tag: tag);
   }
-  void loginOut(){
+
+  void loginOut() {
     LogUtil.e("退出 ", tag: tag);
     Navigator.of(context).pop('loginOut');
   }
-  void statistics(){
+
+  void statistics() {
     LogUtil.e("statistics ", tag: tag);
   }
-
 }
